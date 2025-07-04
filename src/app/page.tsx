a70551805916cd5a33f1963247f91fe344089a56
shubham <70461738+Shubham-yelekar@users.noreply.github.com>
2025-07-04 13:08:55 +0530

    init

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="max-w-[1200px] m-auto mt-32">
+      <h1 className="text-3xl font-sans font-medium  ">Hello world!</h1>
+      <p className="font-serif ">
+        For you to init your SDK and interact with Appwrite services you need to
+        add a web platform to your project. To add a new platform
+      </p>
+    </div>
+  );
+}
